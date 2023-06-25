7a6238354112c2f61cd62855c626a7744e66b79f
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-06-26 01:47:30 +0700

    commit4

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import *
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('contacts/', ContactCreateView.as_view(), name='contacts'),
+    path('catalog/', ProductsListView.as_view(), name='catalog'),
+    path('product_items/<int:pk>/', ProductDetailView.as_view(), name='product_items'),
+    path('product/create/', ProductCreateView.as_view(), name='save_product'),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from catalog.models import Product, Contact
 
 
-def home(request):
-    return render(request, 'catalog/home.html')
+class ProductsListView(generic.ListView):
+    model = Product
+    extra_context = {
+        "title": "Каталог товаров"
+    }
 
 
-def contacts(request):
-    return render(request, 'catalog/contact.html')
+class ProductDetailView(generic.DetailView):
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = context_data['product']
+        return context_data
+
+
+class ContactCreateView(generic.CreateView):
+    model = Contact
+    fields = ('name', 'phone', 'message')
+
+
+class ProductCreateView(generic.CreateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'category', 'price', 'created_date', 'changed_date')
+    success_url = reverse_lazy('catalog:catalog')
