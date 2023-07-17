1dae1ff8bef17e2a96b0a0dcf85e73ee5363bcbf
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-07-18 01:26:02 +0700

    commit7.

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('catalog/', include('catalog.urls', namespace='catalog')),
+                  path('users/', include('users.urls', namespace='users')),
                   path('', include('blog.urls', namespace='blog')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
